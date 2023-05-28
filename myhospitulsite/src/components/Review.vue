d6dc84c3475b0c91d3a0fdda2a32b5e60c48f2dc
chimaobim09 <105109245+chimaobim09@users.noreply.github.com>
2023-05-28 19:38:05 +0100

    updated
    finished with the review page

--- myhospitulsite/src/components/Review.vue
+++ myhospitulsite/src/components/Review.vue
@@ -1,31 +1,66 @@
 <template>
   <center><p id="into" class="mt-5 pb-2">OUR REVIEWS THUS FAR....</p></center>
-  <div class="container mb-4" style="height: 280px;">
+  <div class="container contain mb-4">
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="2000">
       <div class="imgz">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis reiciendis dolores asperiores velit nisi iure id, non perspiciatis obcaecati hic suscipit repudiandae dolor magni laborum. Praesentium ipsam suscipit facilis omnis!</p>
+        <center><div class="container">
+          <div class="contain-circle">
+            <img src="../assets/person-circle.svg" class="imgg"/>
+          </div>
+          <p class="tex">Odunukwe Blessing Nwabugo</p>
+          <p class="tex">Goodheart Medical Consultants Hospital</p>
+          <p class="tex">This is the best thing that happened to Goodheart. Easy to learn and it makes work faster. Kudos</p>
+        </div></center>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
         <div class="imgz">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis reiciendis dolores asperiores velit nisi iure id, non perspiciatis obcaecati hic suscipit repudiandae dolor magni laborum. Praesentium ipsam suscipit facilis omnis!</p>
+          <center><div class="container">
+          <div class="contain-circle">
+            <img src="../assets/person-circle.svg" class="imgg"/>
+          </div>
+          <p class="tex">Elechi Patience</p>
+          <p class="tex">Goodheart Medical Consultants Hospital</p>
+          <p class="tex">A good app.... Fast response.</p>
+        </div></center>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
         <div class="imgz">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis reiciendis dolores asperiores velit nisi iure id, non perspiciatis obcaecati hic suscipit repudiandae dolor magni laborum. Praesentium ipsam suscipit facilis omnis!</p>
+          <center><div class="container">
+          <div class="contain-circle">
+            <img src="../assets/person-circle.svg" class="imgg"/>
+          </div>
+          <p class="tex">Ogunka Gold</p>
+          <p class="tex">KARIDEN SPECIALIST HOSPITAL HOSPITAL</p>
+          <p class="tex">The app is slow. The template is not organized as it ought to be for a hospital software.</p>
+        </div></center>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
         <div class="imgz">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis reiciendis dolores asperiores velit nisi iure id, non perspiciatis obcaecati hic suscipit repudiandae dolor magni laborum. Praesentium ipsam suscipit facilis omnis!</p>
+          <center><div class="container">
+          <div class="contain-circle">
+            <img src="../assets/person-circle.svg" class="imgg"/>
+          </div>
+          <p class="tex">Odunukwe Blessing Nwabugo</p>
+          <p class="tex">Goodheart Medical Consultants Hospital</p>
+          <p class="tex">This is the best thing that happened to Goodheart. Easy to learn and it makes work faster. Kudos</p>
+        </div></center>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
         <div class="imgz">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis reiciendis dolores asperiores velit nisi iure id, non perspiciatis obcaecati hic suscipit repudiandae dolor magni laborum. Praesentium ipsam suscipit facilis omnis!</p>
+          <center><div class="container">
+          <div class="contain-circle">
+            <img src="../assets/person-circle.svg" class="imgg"/>
+          </div>
+          <p class="tex">Dr. Edwin Chukwudi</p>
+          <p class="tex">Clared Hospitals LTD</p>
+          <p class="tex">Satisfactory</p>
+        </div></center>
       </div>
     </div>
   </div>
@@ -49,14 +84,40 @@ name: "ReView"
 
 <style scoped>
 .imgz {
-    height: 260px;
     background-color: rgb(27,129,60);
+    border-radius: 15px;
     text-align: center;
+    height: 300px;
+    padding: 15px;
 }
 #into {
   font-size: 32px;
   font-weight: 500;
   line-height: 40px;
   color:rgb(3, 101, 2);
+}
+.tex {
+  padding: 5px;
+  font-size: 18px;
+  width: 70%;
+  letter-spacing: 2px;
+  color: rgb(247, 249, 247);
+}
+
+.contain-circle{
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+.imgg{
+  width: 70px;
+  height: 70px;
+}
+@media only screen and (max-width: 600px){
+  .tex{
+    width: 100%;
+    font-size: 15px;
+  }
 }
 </style>
